6009b6e94c2d2b6b27f21b0f667e6635c958d1b5
Jiyoon Hur <jiyoon1156@gmail.com>
2020-10-16 21:29:17 +0900

    ADD: 3-color-button

--- styled/styling-with-styled-components/src/App.js
+++ styled/styling-with-styled-components/src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+import Button from './components/Button';
 
-const Circle = styled.div`
-  width: 5rem;
-  height: 5rem;
-  background: ${props => props.color || 'black'};
-  border-radius: 50%;
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
 `;
 
 function App() {
-  return <Circle color="blue"/>;
+  return (
+    <ThemeProvider
+      theme={{
+        palette: {
+          blue: '#228be6',
+          gray: '#495057',
+          pink: '#f06595'
+        }
+      }}
+    >
+      <AppBlock>
+        <Button>BUTTON</Button>
+        <Button color="gray">BUTTON</Button>
+        <Button color="pink">BUTTON</Button>
+      </AppBlock>
+    </ThemeProvider>
+  );
 }
 
 export default App;
